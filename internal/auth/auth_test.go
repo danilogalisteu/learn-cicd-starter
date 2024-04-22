1183ed34be7e67d2c2f569b7d07e87f331c4d034
danilogalisteu <danilogalisteu@gmail.com>
2024-04-22 19:04:32 -0300

    added auth tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	headers := http.Header{}
+
+	_, err := GetAPIKey(headers)
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("Error parsing empty auth header:\n%v", err)
+	}
+
+	api_key := "1234567890"
+
+	headers.Set("Authorization", api_key)
+	_, err = GetAPIKey(headers)
+	if (err == ErrNoAuthHeaderIncluded) || (err == nil) {
+		t.Fatalf("Error parsing malformed auth header")
+	}
+
+	headers.Set("Authorization", api_key + " ABCDE")
+	_, err = GetAPIKey(headers)
+	if (err == ErrNoAuthHeaderIncluded) || (err == nil) {
+		t.Fatalf("Error parsing malformed header:\n%v", err)
+	}
+
+	headers.Set("Authorization", api_key + " ABCDE FGHIJ")
+	_, err = GetAPIKey(headers)
+	if (err == ErrNoAuthHeaderIncluded) || (err == nil) {
+		t.Fatalf("Error parsing malformed header:\n%v", err)
+	}
+
+	headers.Set("Authorization", "ApiKey " + api_key)
+	key, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("Error parsing correct auth header:\n%v", err)
+	}
+
+	if key != api_key {
+		t.Fatalf("Wrong key '%s' was parsed from header; should be '%s'", key, api_key)
+	}
+}
