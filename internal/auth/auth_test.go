9158c301f325efa2b190c23ea9c7e5e1b78bc645
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-13 11:05:44 -0300

    added tests for GetAPIKey

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		name       string
+		authHeader string
+		key        string
+		err        error
+	}
+
+	tests := []test{
+		{name: "empty", authHeader: "", key: "", err: ErrNoAuthHeaderIncluded},
+		{name: "missing key name", authHeader: "p@ssw0rd", key: "", err: ErrMalformedAuthHeader},
+		{name: "wrong key name", authHeader: "apikey p@ssw0rd", key: "", err: ErrMalformedAuthHeader},
+		{name: "correct", authHeader: "ApiKey p@ssw0rd", key: "p@ssw0rd", err: nil},
+	}
+
+	for _, tc := range tests {
+		headers := http.Header{}
+		if tc.authHeader != "" {
+			headers.Set("Authorization", tc.authHeader)
+		}
+		key, err := GetAPIKey(headers)
+		if key != tc.key || !errors.Is(err, tc.err) {
+			t.Fatalf("test '%s' error\nexp: '%v', '%v'\ngot: '%v', '%v'", tc.name, tc.key, tc.err, key, err)
+		}
+	}
+}
